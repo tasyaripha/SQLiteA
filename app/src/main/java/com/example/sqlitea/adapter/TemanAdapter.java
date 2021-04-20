f44f7ad1a3d5ff85e9b4e6370b2001dcdf1e1a0c
Rayka Agustasya <raykaagustasya16@gmail.com>
2021-04-21 05:33:30 +0700

    SQLite 1

---
+++ app/src/main/java/com/example/sqlitea/adapter/TemanAdapter.java
@@ -0,0 +1,56 @@
+package com.example.sqlitea.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sqlitea.R;
+import com.example.sqlitea.database.Teman;
+
+import java.util.ArrayList;
+
+public class TemanAdapter extends RecyclerView.Adapter<TemanAdapter.TemanViewHolder> {
+    public ArrayList<Teman> listData;
+
+    public TemanAdapter(ArrayList<Teman> listData) {
+        this.listData = listData;
+    }
+
+    public TemanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInf = LayoutInflater.from(parent.getContext());
+        View view = layoutInf.inflate(R.layout.row_data_teman,parent,false);
+        return new TemanViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TemanViewHolder holder, int position) {
+        String nm, tlp;
+
+        nm = listData.get(position).getNama();
+        tlp = listData.get(position).getTelpon();
+
+        holder.namaTxt.setText(nm);
+        holder.telponTxt.setText(tlp);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (listData != null)?listData.size() : 0;
+    }
+
+    public class TemanViewHolder extends RecyclerView.ViewHolder {
+        private CardView cardku;
+        private TextView namaTxt, telponTxt;
+        public TemanViewHolder(@NonNull View view) {
+            super(view);
+            cardku = (CardView) view.findViewById(R.id.kartuku);
+                    namaTxt = (TextView) view.findViewById(R.id.textNama);
+                    telponTxt = (TextView) view.findViewById(R.id.textTelpon);
+        }
+    }
+}
